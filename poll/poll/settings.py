322c05b214d3700afbc54c6ac03233216d04fb6d
Olivia Helmuth <Olivia.Helmuth@pm.me>
2020-09-30 19:58:08 +0200

    chore: allowed_hosts for heroku

--- poll/poll/settings.py
+++ poll/poll/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://olivia-helmuth-pollapp.herokuapp.com/', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['olivia-helmuth-pollapp.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
